816894e3ecc6ac1d761a6d7d0e2bbd481e2f30f5
Developer dani <Dani.ramirez2305@gmail.com>
2025-10-19 20:06:11 -0500

    Initial commit

---
+++ app/src/main/java/com/ucompensar/petrescue/activities/ResgistroUsuarioActivity.kt
@@ -0,0 +1,17 @@
+package com.ucompensar.petrescue.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.ucompensar.petrescue.R
+
+class ResgistroUsuarioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_resgistro_usuario)
+
+    }
+}
